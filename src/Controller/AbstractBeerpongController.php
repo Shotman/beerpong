ae3c7a3ffbc0f3fde9d1bb827fc24cb6859176ba
Shotmann <sylvian.ropert@gmail.com>
2024-02-14 23:37:47 +0100

    Change BDD scope + cookie lifetime

--- src/Controller/AbstractBeerpongController.php
+++ src/Controller/AbstractBeerpongController.php
@@ -10,21 +10,26 @@ abstract class AbstractBeerpongController extends AbstractController
 {
     public function __construct(Security $security, EntityManagerInterface $em)
     {
+        //Bar admin
         if(!is_null($security->getUser()) && in_array("ROLE_ADMIN",$security->getUser()->getRoles()))
         {
             $em->getConfiguration()->addFilter('admin', 'App\Filters\AdminFilter');
             $filter = $em->getFilters()->enable('admin');
             $filter->setParameter('admin', $security->getUser()->getId());
         }
+        //Guest
         if($security->getUser() === null){
             $em->getConfiguration()->addFilter('admin', 'App\Filters\AdminFilter');
             $filter = $em->getFilters()->enable('admin');
             $filter->setParameter('admin', NULL);
         }
+        //Super admin
         if(!is_null($security->getUser()) && in_array("ROLE_SUPER_ADMIN",$security->getUser()->getRoles()))
         {
             $em->getConfiguration()->addFilter('admin', 'App\Filters\AdminFilter');
-            $em->getFilters()->disable('admin');
+            if($em->getFilters()->isEnabled("admin")){
+                $em->getFilters()->disable('admin');
+            }
         }
     }
 }
